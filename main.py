e87f3163460137d67ef3cf5f671f5c235ca8e8bb
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2023-02-21 11:16:30 -0600

    Implementacion de BFS para dibujar correctamente el AFN

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 import pydot
 from Postfix_converter import *
 from Thompson import *
-converter = Postfix_converter("(a|b)|c+")
+from collections import *
+
+
+
+
+converter = Postfix_converter("(a|b)*|(c|d)*")
+print(converter.postfix)
 thomp = Thompson(converter.postfix)
 thomp_afn = thomp.construction()
 thomp_afn[0].start = True
 thomp_afn[1].end = True
 
+
 afn = pydot.Dot(graph_type='digraph', rankdir='LR')
 
+first_node = thomp_afn[0]
+ordered_nodes = []
+visited_nodes = {first_node}
+print(visited_nodes)
+queue = deque([first_node])
+while queue:
+    node = queue.popleft()
+    ordered_nodes.append(node)
+    for i in node.transitions.keys():
+        for j in node.transitions[i]:
+            if j not in visited_nodes:
+                for k in thomp.route:
+                    if k.value == j:
+                        visited_nodes.add(j)
+                        queue.append(k)
+                    
 
-for i in thomp.route:
+
+for i in ordered_nodes:
     if i.end:
         temp_node = pydot.Node(str(i.value), peripheries = '2')
     else:
         temp_node = pydot.Node(str(i.value))
     afn.add_node(temp_node)
 
-for i in thomp.route:
+for i in ordered_nodes:
     for j in i.transitions.keys():
         for k in i.transitions[j]:
             if j == "ε":
